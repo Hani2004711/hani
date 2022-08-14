346a845ef3dc2eac0801e157f903aed186d6f901
Hani Al-Bakhiti <111097690+Hani2004711@users.noreply.github.com>
2022-08-14 11:48:26 -0900

    Add files via upload

---
+++ Homework ;Lab On hani Al Bokhiti/S5/src/S5.java
@@ -0,0 +1,10 @@
+public class S5
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("##################");
+        System.out.println("|Hani AL-Bakhiti|");
+        System.out.println("##################");
+    }
+}
---
+++ Homework ;Lab On hani Al Bokhiti/S7/src/S7.java
@@ -0,0 +1,12 @@
+public class S7
+{
+    public static void main(String[]args)
+    {
+        //البيت تعاب عمله
+        System.out.println("Nani");
+        System.out.println("  +");
+        System.out.println("+   +");
+        System.out.println("+    +");
+        System.out.println("+____+");
+    }
+}
